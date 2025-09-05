cea116093483402d9bbc33fc11684e2e4bbe0fde
kipiiler <hoanghohohaha@gmail.com>
2025-09-04 22:25:04 -0700

    update with correct submodule

--- backend-server/app/models/rabbit_message.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum as PyEnum
-
-class SimulationMessageType(str, PyEnum):
-    """
-    Enum for simulation message types.
-    """
-    RUN = "RUN"
-    RUN_USER = "RUN_USER"
-    SCALE_DOCKER = "SCALE_DOCKER"
--- backend-server/app/service/docker/__init__.py
+++
@@ -1,1 +0,0 @@
-# Docker service module initialization
